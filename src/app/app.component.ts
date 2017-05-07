dcc2a3159f9602b81d49930080f51daaae370ac0
justpar72 <hieuqn@yahoo.com>
2017-05-07 12:45:09 -0500

    add author

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}{{author}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    name = 'Angular';
+    author = ' by Hieu';
+}
